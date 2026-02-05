1e7d345fb1424b565b2853f0eff4795c1a525e71
Giorgi Kostava <gkostava03@gmail.com>
2026-02-06 00:25:03 +0400

    feat: add hover details

--- src/components/img component/img.jsx
+++ src/components/img component/img.jsx
@@ -1,8 +1,17 @@
 import "./img.css";
 import Photo from "../../assets/Rectangle Copy (1).png";
+import Eye from "../../assets/Shape (7).png";
 
 function Image() {
-  return <img className="img_1" src={Photo} alt="" />;
+  return (
+    <div className="img_div">
+      <img className="img_1" src={Photo} alt="" />
+
+      <div className="sec_div">
+        <img className="eye_icon" src={Eye} alt="hidephoto" />
+      </div>
+    </div>
+  );
 }
 
 export default Image;
